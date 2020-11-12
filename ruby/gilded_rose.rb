303607a86b9d54b8240ef4fbb3c79ccba54565fa
prostko <eprostko@optoro.com>
2020-11-12 15:25:46 -0500

    day 1: updated if statements, extracted to methods, gained a better feeling for the code

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -6,49 +6,53 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      item.decrement_quality unless item.aged_brie? || item.sulferas? || item.backstage_pass?
+      item.increment_quality if item.aged_brie? || item.backstage_pass?
+      item.sell_in -= 1 unless item.sulferas?
+
+      #   if !item.aged_brie? && !item.backstage_pass?
+      #     if item.quality > 0
+      #       if !item.sulferas?
+      #         item.decrement_quality
+      #       end
+      #     end
+      #   else
+      #     if item.quality < 50
+      #       item.increment_quality
+
+      #       if item.backstage_pass?
+      #         if item.sell_in < 11 && item.quality < 50
+      #           item.increment_quality
+      #         end
+
+      #         if item.sell_in < 6 && item.quality < 50
+      #           item.increment_quality
+      #         end
+      #       end
+      #     end
+      #   end
+
+
+      #   if !item.sulferas?
+      #     item.sell_in = item.sell_in - 1
+      #   end
+
+
+      #   if item.sell_in < 0
+      #     if !item.aged_brie?
+      #       if !item.backstage_pass?
+      #         if item.quality > 0 && !item.sulferas?
+      #           item.decrement_quality
+      #         end
+      #       else
+      #         item.zero_quality
+      #       end
+      #     else
+      #       if item.quality < 50
+      #         item.increment_quality
+      #       end
+      #     end
+      # end
     end
   end
 end
@@ -62,7 +66,34 @@ class Item
     @quality = quality
   end
 
-  def to_s()
+  def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
+  end
+
+  def increment_quality
+    @quality += 1
+  end
+
+  def decrement_quality
+    @quality -= 1
+  end
+
+  def zero_quality
+    @quality = 0
+  end
+
+  def aged_brie?
+    name == "Aged Brie"
+  end
+
+  def backstage_pass?
+    name == "Backstage passes to a TAFKAL80ETC concert"
+  end
+
+  def sulferas?
+    name == "Sulfuras, Hand of Ragnaros"
+  end
+
+  def update_quality
   end
 end
